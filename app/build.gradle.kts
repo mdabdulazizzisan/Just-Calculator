3e1b34c64b6625a69274fcfa73eedd5d908ddae0
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-06-30 07:48:35 -0400

    fixed material outlined button animation in build.gradle.kts and buttons_layout.xml
    added lavender colors in colors.xml and themes.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.material.v1130alpha03)
+
+
 }
